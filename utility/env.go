6129cc8597048182b1e63e7b3274971b108fb5ed
Mohit Udupa <moudupa@cisco.com>
2021-02-06 19:42:15 +0530

    Added ennv variable MAX_STREAMS. Tweaked defaults for TIMBER_DATA

--- utility/env.go
+++ utility/env.go
@@ -7,14 +7,16 @@ import (
 )
 
 const (
-	timberData = "~/.timber"
+	timberData = ".timber"
 	timberPort = 36036
+	maxStreams = 32
 )
 
 // Environment holds sanitized environment variables
 type Environment struct {
 	TimberData string
 	TimberPort int
+	MaxStreams int
 }
 
 // GetEnv reads environment variables, sanitizes them and returns an Environment struct
@@ -23,6 +25,7 @@ func GetEnv() (*Environment, error) {
 	env := &Environment{
 		TimberData: timberData,
 		TimberPort: timberPort,
+		MaxStreams: maxStreams,
 	}
 	// Read TIMBER_DATA enviroment
 	data := os.Getenv("TIMBER_DATA")
@@ -41,6 +44,19 @@ func GetEnv() (*Environment, error) {
 			return nil, errors.New("TIMBER_PORT must be an within the range of 0 - 65535")
 		}
 		env.TimberPort = port
+	}
+
+	// Read MAX_STREAMS environment
+	streamsS := os.Getenv("MAX_STREAMS")
+	if streamsS != "" {
+		streams, err := strconv.Atoi(streamsS)
+		if err != nil {
+			return nil, errors.New("MAX_STREAMS must be an integer")
+		}
+		if streams <= 0 {
+			return nil, errors.New("MAX_STREAMS must be an greater than 0")
+		}
+		env.MaxStreams = streams
 	}
 
 	return env, nil
