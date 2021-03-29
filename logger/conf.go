8b2a55a7aded9530957d3b065e7c25700852c3dd
Mohit Udupa <moudupa@cisco.com>
2021-03-29 23:09:17 +0530

    Added conf.go to load timber-config from a JSON file

---
+++ logger/conf.go
@@ -0,0 +1,41 @@
+package logger
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+const (
+	defaultData = "./timber"
+	defaultPort = 36036
+	defaultLogs = "default.log"
+)
+
+type Config struct {
+	Data string   `json:"data"`
+	Port int      `json:"port"`
+	Logs []string `json:"logs"`
+}
+
+func NewConfig() *Config {
+	conf := &Config{
+		Data: defaultData,
+		Port: defaultPort,
+		Logs: []string{defaultLogs},
+	}
+	return conf
+}
+
+func (c *Config) Load() error {
+	f, err := ioutil.ReadFile("./timberconf.json")
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(f, c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
